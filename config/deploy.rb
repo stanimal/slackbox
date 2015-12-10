f728a23b5eec8ed45bc74c8301cb681368776bb2
Stan Cheng <stan@jewlr.com>
2015-12-10 13:55:12 -0500

    Added .env to symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :npm_flags, '--silent --no-spin'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, %w(server/config/auth.js)
+set :linked_files, %w(server/config/auth.js .env)
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
